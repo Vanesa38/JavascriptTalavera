8d80ce7f404cb7392d300a856cc754ecd10114c5
vane_talavera <vanetala32@gmail.com>
2022-07-21 22:06:20 -0300

    javascript

---
+++ codigo.js
@@ -0,0 +1,7 @@
+for(let i=1;i<=5; i++){
+    // En cada repeticion se solicita el nombre.
+let ingresarNombre = prompt("ingresar nombre");
+   //  Informamos el turno asignado
+alert(" Turno Nº "+i+" Nombre: "+ingresarNombre);   
+
+}
