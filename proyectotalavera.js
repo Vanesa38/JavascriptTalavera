df1d5ceb5643670834e9b7c42e26ade1e6994c2c
vane_talavera <vanetala32@gmail.com>
2022-08-03 19:28:02 -0300

    proyectotalavera

--- proyectotalavera.js
+++ proyectotalavera.js
@@ -1,37 +1,42 @@
+function saludar() {
+    alert("Bienvenido a Habito")
+}
+
+saludar ();
+
 let producto = prompt ("Ingresa el producto para saber su precio");
 let precio =0;
-
 function calcular(){
 
     while(producto!="fin"){
         switch(producto){
             case "Leche de Almendras y coco 1lt":
-                precio=precio + "$" +500;
+                precio=precio+500;
                 console.log("la leche de almendras cuesta $500");
                 console.log("El articulo se encuentra en stock");
                 break;
             case "Jugo Arandanos 1.5lt c/stevia":
-                precio=precio + "$" +150;
+                precio=precio+150;
                 console.log("El jugo de Arandanos cuesta $150");
                 console.log("El articulo se encuentra en stock");
                 break;
             case "Kefir 1lt Gaia Vegan":
-                precio=precio + "$" +350
+                precio=precio+350
                 console.log("Kefir cuesta $350");
                 console.log("El articulo se encuentra en stock");
                 break;
             case "Vita Biosa Probiota Sabor Natural 500ml":
-                precio=precio + "$" +1180
+                precio=precio+1180
                 console.log("Vita Biosa cuesta $1180");
                 console.log("El articulo se encuentra en stock");
                 break;
             case "Arrope de Mistol 430g":
-                precio=precio + "$" +360
+                precio=precio+360
                 console.log("El arrope de mistol cuesta $360");
                 console.log("El articulo se encuentra en stock");
                 break;
             case "Pasta De Maní Natural 380g Hardy":
-                precio=precio + "$" +460
+                precio=precio+460
                 console.log("La pasta de mani cuesta $460");
                 console.log("El articulo se encuentra en stock");
                 break;
@@ -39,12 +44,12 @@ function calcular(){
                 console.log("El articulo no se encuentra en stock");
                 break;
             case "Cerveza de Mijo Rubia en Lata 473ml Gauther":
-                precio=precio + "$" +350
+                precio=precio+350
                 console.log("La cerveza de mijo cuesta $350");
                 console.log("El articulo se encuentra en stock");
                 break;
             case "Fernet Yuyo 750 ml":
-                precio=precio + "$" +800
+                precio=precio+800
                 console.log("El fernet cuesta $800");
                 console.log("El articulo se encuentra en stock");
                 break;
@@ -52,7 +57,7 @@ function calcular(){
                 console.log("Este articulo no se encuentra disponible");
                 break;
         }
-        producto=prompt("Ingresa el producto para saber su precio (fin para ver el total).");
+        producto=prompt("Ingresa el producto para saber su precio.");
     }
 
     console.log("Total a pagar $"+precio);
@@ -98,4 +103,9 @@ for (const productos of productos){
 
 productos.push("Jalapeño Ahumado 180ml");
 console.log(productos);
+
+const oferta = productos.filter(producto => producto.precio < 1000)
+console.log(oferta)
+
+
 
