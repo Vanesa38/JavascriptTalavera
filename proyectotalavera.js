8e23520efb8fc53a6f7d7d4ac2b6a68c4be2568a
vane_talavera <vanetala32@gmail.com>
2022-08-10 23:18:01 -0300

    proyectotalavera

--- proyectotalavera.js
+++ proyectotalavera.js
@@ -35,21 +35,29 @@ for(const imagen of listapromociones){
     card.className="card col-3 text-align-center";
     card.innerHTML=`
     <div class="card" style="width: 18rem;">
-    <img src= ${imagen.fuente} class="card-img-top" alt="...">
+    <img src= ${imagen.promociones} class="card-img-top" alt="promociones">
+    <div class="card-body">
             <h5 class="card-title">${imagen.nombre}</h5>
             <p class="card-text">${imagen.precio}</p>
-            <a href="#" id = "Boton" class="btn btn-primary">COMPRAR</a>
+            <button id="miBoton" class="btn btn-primary">Comprar</button>
+        </div>
         </div>
     `;
     card.append(cartas);
 }
-let boton = document.getElementsByClassName("btn")
+let boton = document.getElementsByClassName("miBoton")
+
+boton.onclick = () => {
+    console.log("Agregaste la promo al carrito");
+    carrito.push(promo);
+    console.table(carrito);
+}
 
 // Incorporando arrays //
 
-for(let i=0;i<5;i++){
-    console.log(productos[i]);
-}
+//for(let i=0;i<5;i++){
+    //console.log(productos[i]);
+//}
 
 class productosiva {
     constructor(nombre, precio) {
@@ -76,20 +84,20 @@ const listaproductos=[
         precio: "1000"
     }
 ];
-for (const productos of productos){
-     console.log(productos.precio);
+//for (const productos of productos){
+     //console.log(productos.precio);
 
-}
+//}
 
 //producto agregado al stock//
 
-productos.push("Jalapeño Ahumado 180ml");
-console.log(productos);
+//productos.push("Jalapeño Ahumado 180ml");
+//console.log(productos);
 
 //productos de oferta//
 
-const oferta = productos.filter(producto => producto.precio < 1000)
-console.log(oferta)
+//const oferta = productos.filter(producto => producto.precio < 1000)
+//console.log(oferta)
 
 
 
