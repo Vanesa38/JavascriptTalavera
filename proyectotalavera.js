81661a2fb561199bbf82890996ffeab7924ae784
vane_talavera <vanetala32@gmail.com>
2022-08-11 21:20:53 -0300

    proyectotalavera

--- proyectotalavera.js
+++ proyectotalavera.js
@@ -1,31 +1,28 @@
 let carrito=[];
 
-
 let cards=document.getElementById("productos"); 
     for (const imagen of listaproductos) {
         let card=document.createElement("div");
         card.className="card col-3 text-align-center";
         card.innerHTML=`
         <div class="card" style="width: 18rem;">
-        <img src= ${imagen.productos} class="card-img-top" alt="...">
-        <h5 class="card-title">${imagen.foto}</h5>
-        <p class="card-text">${imagen.nombre}</p>
-        <button id="miBoton--${imagen.precio}" class="btn btn-primary">COMPRAR</button>
+        <img src= ${imagen.foto} class="card-img-top" alt="...">
+        <h5 class="card-title">${imagen.nombre}</h5>
+        <p class="card-text">${imagen.precio}</p>
+        <button id="miBoton--${imagen.nombre}" class="btn btn-primary">COMPRAR</button>
     </div>
 `;
  cards.append(card);
-
-}
 //Agregar productos al carrito//
-
 let miboton = document.getElementById(`btn${imagen.nombre}`);
-
 miBoton.addEventListener("click", (e) => {
     e.preventDefault();
     console.log("Agregaste" + " " + producto.nombre + " " +"al carrito");
     productos.push(producto);
 })
 
+
+}
 //fin de compra//
 let botonFinalizarCompra = document.getElementById("fin")
 let precioFinal = 0
