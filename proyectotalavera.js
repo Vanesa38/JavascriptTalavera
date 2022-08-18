49a22fdd7778608a3851cbb1bdb2f3d881333042
vane_talavera <vanetala32@gmail.com>
2022-08-18 19:11:10 -0300

    proyectotalavera

--- proyectotalavera.js
+++ proyectotalavera.js
@@ -1,22 +1,49 @@
 let carrito=[];
 
+let compra=document.getElementsByClassName("productos");
+console.log(compra.innerHTML);
+
 let cards=document.getElementById("productos"); 
-    for (const imagen of listaproductos) {
+    for (const compra of listaproductos) {
         let card=document.createElement("div");
         card.className="card col-3 text-align-center";
         card.style= "width: 18rem;";
         card.innerHTML+=`
         <div class="card" style="width: 18rem;">
-        <img src= ${imagen.foto} class="card-img-top" alt="...">
+        <img src= ${compra.foto} class="card-img-top" alt="...">
         <div class="card-body">
-        <h5 class="card-title">${imagen.nombre}</h5>
-        <p class="card-text">${imagen.precio}</p>
-        <button id="btn" class="btn btn-primary" onclick="AddToCart(${imagen.id})">COMPRAR</button>
+        <h5 class="card-title">${compra.nombre}</h5>
+        <p class="card-text">${compra.precio}</p>
+        <button id="btn" class="btn btn-primary" onclick="AddToCart(${compra.id})">COMPRAR</button>
 </div>
 `;                                              //Linea 14. Llamo a la funcion y le envio el id del producto por parametro
 cards.append(card);
 
 }
+
+
+
+//Agregar productos al carrito//
+function AddToCart(selectedId) //recibo el id
+{
+    //metodo find me retorna un objeto que coincida con la condicion
+    let selectedProduct = listaproductos.find(element => element.id == selectedId)
+    if(selectedProduct != null) //valido que no sea null, osea, que encontro algo
+    {
+        carrito.push(selectedProducts) //lo agrgeo
+    }
+    console.log(selectedProducts); //lo muestro por consola para corroborar
+}
+
+
+/*
+//finalizar compra
+let productos = 0
+let botonfinalizarcompra = document.getElementById("productos")
+let precioFinal = 0
+let checkOut = 0
+*/
+
 
 //Agregar productos al carrito//
 function AddToCart(selectedId) //recibo el id
@@ -29,19 +56,14 @@ function AddToCart(selectedId) //recibo el id
     }
     console.log(selectedProduct); //lo muestro por consola para corroborar
 }
-
-
-
 //finalizar compra
-let botonFinalizarCompra = document.getElementById("fin")
-let precioFinal = 0
-let checkOut = 0
-botonFinalizarCompra.addEventListener ("click", (e) => {
+
+productos.addEventListener ("clickcompra", (e) => {
     e.preventDefault ();
-    precioFinal = productos.map (productos => productos.precio)
+    precioFinal = productos.map (selectedproduct => productos.precio)
     checkOut = precioFinal.reduce ((ac,el) => ac + el,0)
     console.table(productos);
-    const oferta = productos.filter (producto => producto.precio < 1000)
+    const oferta = productos.filter (selectedproduct => producto.precio < 1000)
     console.log ("Los siguientes productos estan en oferta")
     console.log (oferta)
 
